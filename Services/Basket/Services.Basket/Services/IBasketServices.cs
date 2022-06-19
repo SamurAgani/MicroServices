5a150364bd7b8c543d02bbc4e0dc4a36dd97323a
SamurAgani <aganiyevsamur@gmail.com>
2022-06-19 17:40:56 +0400

    Redis Added

---
+++ Services/Basket/Services.Basket/Services/IBasketServices.cs
@@ -0,0 +1,16 @@
+﻿using FreeCource.Shared.Dtos;
+using Services.Basket.DTOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Services.Basket.Services
+{
+    public interface IBasketServices
+    {
+        Task<Response<BasketDto>> GetBasket(string userId);
+        Task<Response<bool>> Delete(string userId);
+        Task<Response<bool>> SaveOrUpdate(BasketDto basketDto);
+    }
+}
---
+++ Services/Basket/Services.Basket/Services/RedisService.cs
@@ -0,0 +1,26 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Services.Basket.Services
+{
+    public class RedisService
+    {
+        public readonly string _host;
+        public readonly int _port;
+
+        private ConnectionMultiplexer connectionMultiplexer;
+
+        public RedisService(string host, int port)
+        {
+            _host = host;
+            _port = port;
+        }
+        public void Connect() => connectionMultiplexer = ConnectionMultiplexer.Connect($"{_host}:{_port}");
+
+        public IDatabase GetDb(int db = 1) => connectionMultiplexer.GetDatabase(db);
+
+    }
+}
