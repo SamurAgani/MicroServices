c6a2b46a06d92716a103a56aa264aaf6c7667893
SamurAgani <aganiyevsamur@gmail.com>
2022-07-24 11:35:25 +0400

    OrderApi

---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Order.cs
@@ -0,0 +1,36 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    public class Order : Entity,IAggregateRoot
+    {
+        public DateTime CreatedDate { get; private set; }
+        public Address Address { get; private set; }
+        public string BuyerId { get; private set; }
+
+        private readonly List<OrderItem> _orderItem;
+        public IReadOnlyCollection<OrderItem> orderItems=> _orderItem;
+
+        public Order(string buyerId,Address address)
+        {
+            _orderItem = new List<OrderItem>();
+            CreatedDate = DateTime.Now;
+            BuyerId = buyerId;
+            Address = address;
+        }
+        public decimal GetTotalPrice => _orderItem.Sum(x => x.Price);
+        public void AddOrderItem(string productId,string productName,decimal price, string pictureUrl)
+        {
+            var existProduct = _orderItem.Any(x => x.ProductId == productId);
+            if (!existProduct)
+            {
+                _orderItem.Add(new OrderItem(productId, productName, pictureUrl, price));
+            }
+        }
+    }
+}
---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/OrderItem.cs
@@ -0,0 +1,32 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    public class OrderItem : Entity
+    {
+        public OrderItem(string productId, string productName, string productUrl, decimal price)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            ProductUrl = productUrl;
+            Price = price;
+        }
+
+        public string ProductId { get; private set; }
+        public string ProductName { get; private set; }
+        public string ProductUrl { get; private set; }
+        public Decimal Price { get; private set; }
+
+        public void UpdateOrderItem(string productId, string productName, string productUrl, decimal price)
+        {
+            ProductName = productName;
+            ProductUrl = productUrl;
+            Price = price;
+        }
+    }
+}
