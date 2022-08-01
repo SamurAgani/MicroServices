744b418f36f3ef23dea14b4fede16085695d72ab
SamurAgani <aganiyevsamur@gmail.com>
2022-08-01 21:07:31 +0400

    Payment and gateway add

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -19,6 +19,7 @@ namespace FreeCourse.IdentityServer
             new ApiResource("resource_basket"){ Scopes ={ "basket_fullpermission" } },
             new ApiResource("resource_discount"){ Scopes ={ "discount_fullpermission" } },
             new ApiResource("resource_order"){ Scopes ={ "order_fullpermission" } },
+            new ApiResource("resource_payment"){ Scopes ={ "payment_fullpermission" } },
             new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
 
@@ -39,6 +40,7 @@ namespace FreeCourse.IdentityServer
                 new ApiScope("basket_fullpermission","Basket api API full controll"),
                 new ApiScope("discount_fullpermission","Discount api API full controll"),
                 new ApiScope("order_fullpermission","Discount api API full controll"),
+                new ApiScope("payment_fullpermission","Discount api API full controll"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -60,7 +62,7 @@ namespace FreeCourse.IdentityServer
                     AllowOfflineAccess = true,
                     ClientSecrets = {new Secret("secret".Sha256()) },
                     AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-                    AllowedScopes = { "basket_fullpermission","discount_fullpermission","order_fullpermission",IdentityServerConstants.StandardScopes.Email,IdentityServerConstants.StandardScopes.OpenId,
+                    AllowedScopes = { "basket_fullpermission","payment_fullpermission","discount_fullpermission","order_fullpermission",IdentityServerConstants.StandardScopes.Email,IdentityServerConstants.StandardScopes.OpenId,
                          IdentityServerConstants.StandardScopes.Profile,
                          IdentityServerConstants.StandardScopes.OfflineAccess,IdentityServerConstants.LocalApi.ScopeName,"roles"},
                     AccessTokenLifetime = 1*60*60,
---
+++ Services/FakePayment/FreeCourse.Services.FakePayment/Controllers/FakePaymentController.cs
@@ -0,0 +1,18 @@
+﻿using FreeCource.Shared.ControllerBases;
+using FreeCource.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.FakePayment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakePaymentController : CustomBaseController
+    {
+        [HttpPost]
+        public IActionResult ReceivePayment()
+        {
+            return CreateActionResultInstance<NoContent>(Response<NoContent>.Success(200));
+        }
+    }
+}
