e6c68618df533f5667705bc7402d2bd37aac1030
SamurAgani <aganiyevsamur@gmail.com>
2022-08-05 21:40:13 +0400

    ClientSide

---
+++ Frontends/FreeCourse.Web/Models/SignInInput.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeCourse.Web.Models
+{
+    public class SignInInput
+    {
+        [Display(Name = "Email address")]
+        public string Email { get; set; }
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [Display(Name = "Remmember me")]
+        public bool IsRemember { get; set; }
+    }
+}
